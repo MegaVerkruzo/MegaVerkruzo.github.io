bbc17bb685c914b080d01306b2aedb3adde724c8
MegaVerkruzo <robochat@mail.ru>
2021-06-12 19:23:37 +0300

    Завершил верстать messages

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,7 +1,8 @@
 import s from './Dialogs.module.css';
+import one_post from './../Posts/OnePost/OnePost.module.css'
 import Dialog from "./Dialog/Dialog.js";
 import './../App.css'
-
+import OnePost from './../Posts/OnePost/OnePost'
 
 const Dialogs = () => {
     return (
@@ -17,8 +18,12 @@ const Dialogs = () => {
                 <Dialog name="Sergey"/>
                 <Dialog name="Airat"/>
             </div>
-            <div className={s.messages}>
-                Fdsfsdf
+            <div className={one_post.messages}>
+                <OnePost message="Hi, i waant learn ReactJS"/>
+                <OnePost message="Like"/>
+                <OnePost message="Wow, this is cool"/>
+                <OnePost message="3"/>
+                <OnePost message="76"/>
             </div>
         </div>
     );
