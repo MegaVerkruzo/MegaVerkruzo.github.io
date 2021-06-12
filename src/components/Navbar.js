416d2301796514de6bbe917b15333c4f2aec6cde
MegaVerkruzo <robochat@mail.ru>
2021-06-12 09:59:13 +0300

    + images; + style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,19 @@ import React from 'react';
 const Navbar = () => {
     return (
         <nav className='nav'>
-            <div>
+            <div className="block_text">
                 <a>Profile</a>
             </div>
-            <div>
+            <div className="block_text">
                 <a>Messages</a>
             </div>
-            <div>
+            <div className="block_text">
                 <a>News</a>
             </div>
-            <div>
+            <div className="block_text">
                 <a>Music</a>
             </div>
-            <div>
+            <div className="block_text">
                 <a>Settings</a>
             </div>
         </nav>
