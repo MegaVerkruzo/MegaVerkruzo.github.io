62fccf1710ec32b198cdc3754ccd42806e40a436
MegaVerkruzo <robochat@mail.ru>
2021-06-12 19:05:25 +0300

    Сделал половину интерфейса от Messanger

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,5 +1,7 @@
 import s from './Dialogs.module.css';
 import Dialog from "./Dialog/Dialog.js";
+import './../App.css'
+
 
 const Dialogs = () => {
     return (
@@ -9,6 +11,11 @@ const Dialogs = () => {
                     DIALOGS:
                 </div>
                 <Dialog name="Andrey"/>
+                <Dialog name="Alexey"/>
+                <Dialog name="Lena"/>
+                <Dialog name="Darya"/>
+                <Dialog name="Sergey"/>
+                <Dialog name="Airat"/>
             </div>
             <div className={s.messages}>
                 Fdsfsdf
